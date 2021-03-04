9deda145c39ab64d0e9def6d507ac5d110308f8d
Anthony Moe <anthony.moe7@gmail.com>
2021-03-04 11:59:20 -0600

    first commit

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,6 @@
 $( document ).ready( onReady );
+//create a cohort array
+let emirp = [];
 
 function onReady() {
     // runs when script is loaded
@@ -6,7 +8,23 @@ function onReady() {
     //do a few click handles
     $( '#firstPTag' ).on( 'click', pTagClick );
     $( '.otherPTag' ).on( 'click', otherTagClick );
+    $( '#submitButton' ).on( 'click', getInfo );
 }//end onReady
+
+function getInfo(){
+    console.log( 'in getInfo' );
+    //get value from nameIn
+    const name = $( '#nameIn' ).val(); //gets the value from the element
+    //create a user object
+    const user = {
+        name: name
+    }
+    //push user into cohort
+    emirp.push( user );
+    console.log( emirp );
+    //empty the nameIn element
+    $( '#nameIn' ).val( '' );//set the value for this element 
+}//end getInfo
 
 function pTagClick(){
     console.log( 'clicked on 1st pTag' );
